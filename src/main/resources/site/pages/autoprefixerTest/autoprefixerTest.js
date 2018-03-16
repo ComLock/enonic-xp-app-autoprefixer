f09eba2dca9f7838195efafc92d3edabd724d9db
Christian Westgaard <cwe@enonic.com>
2018-03-16 12:44:20 +0100

    Test uniqCss filter

--- src/main/resources/site/pages/autoprefixerTest/autoprefixerTest.js
+++ src/main/resources/site/pages/autoprefixerTest/autoprefixerTest.js
@@ -1,11 +1,22 @@
+var D_F = '.d-f{display:flex}';
+var M_D_F = '@media(min-width:480px){.d-f-w-mi-480{display:flex}}';
+var TEST_STR = '<style type="text/css">' + M_D_F + '</style>' + '\n' +
+'<style type="text/css">' + '\n' +
+' ' + M_D_F + '\n' +
+'  ' + D_F + '\n' +
+'   ' + M_D_F + '\n' +
+'    ' + D_F + '\n' +
+'</style><style type="text/css">' + D_F + '</style>';
+
+
 exports.get = function() {
   return {
-    body: '<html><head><title>Autoprefixer test</title></head><body><h1 class="d-f">Autoprefixer test</h1></body></html>',
+    body: '<html><head><title>Autoprefixer test</title></head><body><h1 class="d-f">Always display: flex;</h1><p class="d-f-w-mi-480">Flex from @media(min-width:480px)</p></body></html>',
     pageContributions: {
-      headBegin: '<style type="text/css">.d-f{display:flex}</style>',
-      headEnd: ['<style type="text/css">.d-f{display:flex}</style>'],
-      bodyBegin: '<style type="text/css">.d-f{display:flex}</style>',
-      bodyEnd: ['<style type="text/css">.d-f{display:flex}</style>']
+      //headBegin: TEST_STR,
+      headEnd: [TEST_STR, TEST_STR]//,
+      //bodyBegin: TEST_STR,
+      //bodyEnd: [TEST_STR]
     } // pageContributions
   }; // return
 } // exports.get
